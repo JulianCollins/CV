bed0f47aa06dea621cf62b10721cb9a5c83c8dd5
Julian Collins <collins.julian@gmail.com>
2020-06-02 18:44:49 +0100

    cv_20200602

---
+++ juliancollins_cv.R
@@ -0,0 +1,14 @@
+## CV 2020
+
+# devtools::install_github("nstrayer/datadrivencv")
+
+library(datadrivencv)
+
+# setup
+datadrivencv::use_datadriven_cv(
+  full_name = "Julian Collins",
+  data_location = "https://docs.google.com/spreadsheets/d/1la_Si6XRRiE3li2VXghGuZp0HCsKIm8ZPiz9XW1fGiU/edit#gid=917338460",
+  pdf_location = "https://github.com/JulianCollins/cv/raw/master/juliancollins_cv.pdf",
+  html_location = "https://github.com/JulianCollins/cv/raw/master/juliancollins_cv.html",
+  source_location = "https://github.com/JulianCollins/cv"
+)
